b6d04500ebef58234200281b865795185dfb9c47
maa <marcodeavila@gmail.com>
2024-07-22 10:28:09 -0300

    Atualização importacao excel

---
+++ .history/app/Http/Controllers/ExcelImport/ExcelImportController_20240722100549.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\ExcelImport;
+
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\TasksImport;
+use App\Http\Controllers\Controller;
+
+
+class ExcelImportController extends Controller
+{
+    public function import(Request $request)
+    {
+
+        $request->validate([
+            'file' => 'required|mimes:xlsx,xls,csv'
+        ]);
+
+        Excel::import(new TasksImport, $request->file('file'));
+
+        return response()->json(['message' => 'Importação realizada com sucesso!']);
+    }
+}
